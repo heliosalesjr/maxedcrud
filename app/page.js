1b5fb16a1271e1a7af04828ed63d5f9bba31fd4d
heliosalesjr <heliosalesjr@gmail.com>
2024-05-04 14:58:33 -0300

    1st commit: all set

---
+++ app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main>
+      <img src="/logo.png" alt="A server surrounded by magic sparkles." />
+      <h1>Welcome to this NextJS Project!</h1>
+      <p>🔥 Let's get started! 🔥</p>
+    </main>
+  );
+}
