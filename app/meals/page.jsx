0ae78b5b226e88289dd91b07961ab0171c86030f
heliosalesjr <heliosalesjr@gmail.com>
2024-05-08 20:44:08 -0300

    outputting data and images with unkown dimensions

--- app/meals/page.jsx
+++ app/meals/page.jsx
@@ -1,10 +1,27 @@
-import Link from "next/link";
+import classes from '@/app/meals/page.module.css'
+import MealsGrid from '@/components/meals/meals-grid'
+import Link from 'next/link'
 
 export default function MealsPage() {
   return (
-    <main>
-      <h1>Meals!</h1>
-      <Link href="/">Back home</Link>
-    </main>
+    <>
+      <header>
+        <h1 className={classes.header}>
+          Great food for <span className={classes.highlight}>you</span>
+        </h1>
+        <p>
+          Choose your favorite recipes and cook it yourself!
+        </p>
+
+        <p className={classes.cta}>
+          <Link href='/meals/share'>
+            Share your favorite recipes!
+          </Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]}/>
+      </main>
+    </>
   )
 }
